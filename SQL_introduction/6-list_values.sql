6c3e21604cc4c950decd459d7d5af86debde16ab
AAkindimeji <141722088+AAkindimeji@users.noreply.github.com>
2024-01-12 14:10:47 +0100

    SQL intro projects

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
+SELECT * FROM first_table;
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
+SELECT COUNT(*) AS number_of_records
+FROM first_table
+WHERE id = 89;
